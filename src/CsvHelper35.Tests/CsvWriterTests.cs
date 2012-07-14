617c93a0564a1400dd59762fa8fbd86bdf8af189
Josh Close <narshe@gmail.com>
2012-07-14 10:36:42 -0500

    Created non-generic overloads for reading, writing, and attribute mapping.

---
+++ src/CsvHelper35.Tests/CsvWriterTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CsvHelper35.Tests
+{
+	public class CsvWriterTests
+	{
+		[Fact]
+		public void WriteRecordNonGenericTest()
+		{
+			
+		}
+	}
+}
